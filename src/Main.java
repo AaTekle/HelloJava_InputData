db8613c2db7ee5d94cfb446ace0ae08188389b63
AaTekle <cryptoaaron1@gmail.com>
2022-11-19 19:52:08 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    // byte > short > int > long > float > double
+    // 10%
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            byte age = scanner.nextByte();
+            System.out.println("You are " + age); //"String" is a byte , Age is declared as a byte///
+
+    }
+}
